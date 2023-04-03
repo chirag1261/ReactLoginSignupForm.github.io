feb2044173867b116ac06beefad2b841834a993e
Chirag Kumar <72370490+chirag1261@users.noreply.github.com>
2023-04-04 00:09:07 +0530

    React Login Signup form using Routes

---
+++ src/pages/Signup.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import signupImg from "../assets/signup.png"
+import Template from '../components/Template'
+
+const Signup = ({setIsLoggedIn}) => {
+  return (
+    <Template
+      title="SignUp Form"
+      desc1="SignUp here to Login the form using Nav components."
+      desc2="Education to future-proof your career."
+      image={signupImg}
+      formtype="signup"
+      setIsLoggedIn={setIsLoggedIn}
+    />
+  )
+}
+
+export default Signup
